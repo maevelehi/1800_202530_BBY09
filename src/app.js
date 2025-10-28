362609f8b15ee7289fb11265c48350e2bb69fc2a
Maeve Le <nguyenmaeve912@gmail.com>
2025-10-27 22:28:52 -0700

    add logo.png, add color for bg-info, add create-card.html

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import "bootstrap";
 //--------------------------------------------------------------
 // If you have custom global styles, import them as well:
 //--------------------------------------------------------------
-// import "/src/styles/style.css";
+import "/src/styles/style.css";
 
 //--------------------------------------------------------------
 // Custom global JS code (shared with all pages)can go here.

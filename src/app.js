d4ab032409af82309e8b7fba1c40a0a3f1d0f90c
Maeve Le <nguyenmaeve912@gmail.com>
2025-11-06 17:45:01 -0800

    add in topic selection

--- src/app.js
+++ src/app.js
@@ -62,61 +62,6 @@ async function loadSavedCards() {
   });
 }
 
-
-// function loadSavedCards() {
-//   const savedCards = JSON.parse(localStorage.getItem("cardData"));
-//   if (!Array.isArray(savedCards) || savedCards.length === 0) return;
-
-//   savedCards.forEach((cardData) => {
-//     const questionCard = document.createElement("div");
-//     questionCard.classList.add("question-card");
-
-//     //determined chapter base on label dynamically
-//     let chapterClass = "";
-//     if(cardData.label === "Chapter 1") {
-//       chapterClass = "chapter-label1";
-//     } else if (cardData.label === "Chapter 2") {
-//       chapterClass = "chapter-label2";
-//     } else if (cardData.label === "Chapter 3") {
-//       chapterClass = "chapter-label3";
-//     } else if (cardData.label === "Chapter 4") {
-//       chapterClass = "chapter-label4";
-//     } else {
-//       chapterClass = "chapter-label5";
-//     } 
-
-//     const chapterLabel = document.createElement("div");
-//     chapterLabel.classList.add(chapterClass);
-//     chapterLabel.textContent = cardData.label;
-//     questionCard.appendChild(chapterLabel);
-
-//     // assign number to cards
-//     const questionText = document.createElement("p");
-//     questionText.textContent = `Q${cardData.front}`;
-//     questionCard.appendChild(questionText);
-
-//     const answerText = document.createElement("p");
-//     answerText.textContent = `A: ${cardData.back}`;
-//     answerText.style.display = "none";
-//     questionCard.appendChild(answerText);
-
-//     const flipBtn = document.createElement("button");
-//     flipBtn.classList.add("flip-btn");
-//     flipBtn.textContent = "Flip";
-//     flipBtn.addEventListener("click", () => {
-//       answerText.style.display = answerText.style.display === "none" ? "block" : "none";
-//     })
-//     questionCard.appendChild(flipBtn);
-
-//     // questionCard.appendChild(chapterLabel, questionText, answerText, flipBtn)
-//     document.querySelector(".questions-list").appendChild(questionCard);
-//   });
-
-//   // cleanup
-//   // localStorage.removeItem("cardData");
-// }
-// document.addEventListener("DOMContentLoaded", loadSavedCards);
-
 document.addEventListener("DOMContentLoaded", () => {
   if (document.querySelector(".questions-list")) loadSavedCards();
 });
--- src/create-card.js
+++ src/create-card.js
@@ -1,46 +1,30 @@
 import { db } from "./firebaseConfig.js";
 import { collection, addDoc } from "firebase/firestore";
 
-// label btn
-const dropdownbtn = document.getElementById("dropdownMenu");
-const dropdownitem = document.querySelectorAll(".dropdown-item");
+// update btn with selected option
+document.querySelectorAll(".drop-down").forEach((drop) => {
+  const btn = drop.querySelector(".dropdown-toggle");
+  const items = drop.querySelectorAll(".dropdown-item");
 
-dropdownitem.forEach(item => {
-  item.addEventListener("click", () => {
-    dropdownbtn.textContent = item.textContent;
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      btn.textContent = item.textContent;
+    });
   });
 });
-
 
 // save button
 const savebtn = document.getElementById("submit");
 
-// savebtn.addEventListener("click", () => {
-//     const data = {
-//         front: document.getElementById("front").value,
-//         back: document.getElementById("back").value,
-//         label: dropdownbtn.textContent.trim(),
-//     }
-
-//     let cards = JSON.parse(localStorage.getItem("cardData"));
-
-//     //if nothing exist, start with empty array
-//     if (!Array.isArray(cards)) cards = [];
-
-//     //append new card
-//     cards.push(data);
-
-//     localStorage.setItem("cardData", JSON.stringify(cards));
-//     // console.log(data);
-//     window.location.href = '/home.html';
-
-// });
-
 savebtn.addEventListener("click", async () => {
+  const chapter = document.getElementById("dropdownMenu").textContent.trim();
+  const course = document.getElementById("dropdownTopic").textContent.trim();
+
   const data = {
     question: document.getElementById("front").value.trim(),
     answer: document.getElementById("back").value.trim(),
-    label: dropdownbtn.textContent.trim(),
+    label: chapter,
+    topic: course,
     group: "set C",
   };
 
